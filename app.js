51457f7a9c685104d71729dfe93edc5f0107532f
Shubh Mehtani <mehtani.shubh@gmail.com>
2022-09-11 02:04:32 +0530

    add security stuff

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== "production"){
     require('dotenv').config();
 }
 
-
 const express= require('express');
 const mongoose = require('mongoose');
 const path= require('path');
@@ -12,6 +11,8 @@ const session= require('express-session');
 const flash= require('connect-flash');
 const passport= require('passport');
 const LocalStrategy= require('passport-local');
+const mongoSanitize= require('express-mongo-sanitize');
+const helmet= require('helmet');
 
 const ExpressError= require('./utils/ExpressError');
 
@@ -45,20 +46,74 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+        replaceWith: '_'
+}));
 
 const sessionConfig= {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
 }};
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+
+const imgSrcUrls=[
+    "https://res.cloudinary.com/shubhmehtani/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+    "https://images.unsplash.com/",
+];
+
+
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: ["'self'", "blob:", "data:", ...imgSrcUrls ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
